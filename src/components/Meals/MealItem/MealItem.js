369264bdbea4b9eee3f8f07c6cb00d2dc10e9e6a
Yeliazar <erik.ruzhakov@mail.ru>
2022-09-26 17:09:02 +0300

    Add Cart Reducer, work with ref & Forward Refs

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,8 +1,22 @@
+import { useContext} from "react";
+
 import MealItemForm from './MealItemForm/MealItemForm';
 import classes from './MealItem.module.css';
+import CartContext from "../../../store/cart-context";
 
 const MealItem = props => {
+    const cartCtx = useContext(CartContext);
+
     const price = `$${props.meal.price.toFixed(2)}`;
+
+    const addToCartHandler = amount => {
+        cartCtx.addItem({
+            id: props.meal.id,
+            name: props.meal.name,
+            amount: amount,
+            price: props.meal.price
+        })
+    }
 
     return (
         <li className={classes.meal}>
@@ -12,7 +26,7 @@ const MealItem = props => {
                 <div className={classes.price}>{price}</div>
             </div>
             <div>
-                <MealItemForm id={props.meal.id}/>
+                <MealItemForm id={props.meal.id} onAddToCart={addToCartHandler}/>
             </div>
         </li>
     );
